4e46ed0738a8c28c77ee84bc7557702de7eb3c7e
Richard Landy <landyrich117@gmail.com>
2022-12-19 14:05:59 -0700

    groupchat

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Project to integrate OpenAI models with an AWS backed chatbot.
 
 Helpful deployment doc here: https://aws.amazon.com/premiumsupport/knowledge-center/lambda-python-package-compatible/
 
-Local Dec Instructions:
+Local Dev Instructions:
 - `cd` into the lamdba functions subdirectory you want to deploy 
 - Run the two pip commands below (in the order they are presented)
     -- NOTE: this is to install openApi and the lamdba compatible binaries for it's pandas dependency
--- twilio-openapi/lambda_function.py
+++ twilio-openapi/lambda_function.py
@@ -13,6 +13,8 @@ init_prompt = """The following is a conversation with an AI assistant.
 
 def lambda_handler(event, context):
     print(event)
+
+    #get prompt from sender and hit openAPI with it
     prompt = event['Body']
 
     response = openai.Completion.create(
@@ -28,7 +30,37 @@ def lambda_handler(event, context):
 
     prompt_response = response["choices"][0]["text"]
 
-    return ('<?xml version=\"1.0\" encoding=\"UTF-8\"?>' +
-           '<Response><Message><Body>' +
-           prompt_response +
-           '</Body></Message></Response>')
+    #get twilio number this message was sent to
+    twilio_number = event['To']
+
+    #collect other message recipients in phonenumbers_in_received_messages list (in the From and OtherRecipientsN fields)
+
+    #collect number of the query sender
+    phonenumbers_in_received_message = [event['From']]
+
+    #collect other message recipients
+    OtherRecipientsN = 0
+    while 'OtherRecipients' + str(OtherRecipientsN) in event:
+        phonenumbers_in_received_message.add(event['OtherRecipients' + str(OtherRecipientsN)])
+        OtherRecipientsN += 1
+
+    #individual message template
+    message_template = """
+        <Message from="%s" to="%s">
+            <Body>
+                %s
+            </Body>
+        </Message>
+    """ 
+
+    #create list of outgoing messages as a single string
+    outgoing_messages = ""
+    for phonenumber_in_received_message in phonenumbers_in_received_message:
+        outgoing_messages += message_template % (twilio_number, phonenumber_in_received_message, prompt_response)
+
+    return ("""
+            <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+            <Response>
+                %s
+            </Response>
+            """ % outgoing_messages)
